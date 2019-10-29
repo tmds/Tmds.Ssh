64100ab1a80abac876e41e757c43e3b792f0f71a
Tom Deseyn <tom.deseyn@gmail.com>
2019-10-29 16:33:00 +0100

    Change license to MIT. (#25)

--- src/Tmds.Ssh/SequencePool.cs
+++ src/Tmds.Ssh/SequencePool.cs
@@ -1,4 +1,4 @@
-// This file is part of Tmds.Ssh which is released under LGPL-3.0.
+// This file is part of Tmds.Ssh which is released under MIT.
 // See file LICENSE for full license details.
 
 using System.Buffers;
