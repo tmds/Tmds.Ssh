621951a672e32a84bca5d15f07a43218514dd3fe
Tom Deseyn <tom.deseyn@gmail.com>
2023-07-28 20:31:30 +0200

    sftp: SftpException: set a human friendly message.

--- src/Tmds.Ssh/SftpException.cs
+++ src/Tmds.Ssh/SftpException.cs
@@ -9,9 +9,21 @@ namespace Tmds.Ssh
     {
         public SftpError Error { get; private set; }
 
-        internal SftpException(SftpError error) : base(error.ToString())
+        internal SftpException(SftpError error) : base(GetSftpExceptionMessage(error))
         {
             Error = error;
         }
+
+        private static string GetSftpExceptionMessage(SftpError error)
+            => error switch
+            {
+                SftpError.NoSuchFile => "The remote path does not exist.",
+                SftpError.PermissionDenied => "Access to the remote path is denied.",
+                SftpError.Unsupported => "The operation is not supported.",
+                SftpError.Eof => "The end of the stream is reached.",
+                SftpError.Failure => "The operation failed.",
+                SftpError.BadMessage => "Invalid argument or path too long.",
+                _ => $"Unknown error: {error}."
+            };
     }
 }
