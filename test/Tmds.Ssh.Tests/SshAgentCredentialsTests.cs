be898befbf255d7fdab147cea6195a5f5c69edff
Tom Deseyn <tom.deseyn@gmail.com>
2024-12-28 07:40:35 +0100

    SshAgentCredentials: add test. (#303)

---
+++ test/Tmds.Ssh.Tests/SshAgentCredentialsTests.cs
@@ -0,0 +1,107 @@
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Tmds.Ssh.Tests;
+
+[Collection(nameof(SshServerCollection))]
+public class SshAgentCredentialsTests
+{
+    private readonly SshServer _sshServer;
+
+    public SshAgentCredentialsTests(SshServer sshServer)
+    {
+        _sshServer = sshServer;
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        using var agent = new SshAgent();
+        agent.Start();
+        agent.Add(_sshServer.TestUserIdentityFile);
+
+        var settings = new SshClientSettings(_sshServer.Destination)
+        {
+            UserKnownHostsFilePaths = [ _sshServer.KnownHostsFilePath ],
+            Credentials = [ new SshAgentCredentials(agent.Address) ]
+        };
+        using var client = new SshClient(settings);
+        await client.ConnectAsync();
+    }
+
+    sealed class SshAgent : IDisposable
+    {
+        private readonly CancellationTokenSource _cts = new();
+        private Process? _sshAgentProcess;
+
+        public string Address { get; }
+
+        public SshAgent()
+        {
+            Address = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        }
+
+        public void Start()
+        {
+            var psi = new ProcessStartInfo()
+            {
+                FileName = "ssh-agent",
+                ArgumentList = { "-c", "-D", "-a", Address },
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+            };
+            _sshAgentProcess = Process.Start(psi);
+        }
+
+        public void Add(string keyFile)
+        {
+            var psi = new ProcessStartInfo()
+            {
+                FileName = "ssh-add",
+                ArgumentList = { "-q", keyFile },
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+            };
+            psi.EnvironmentVariables["SSH_AUTH_SOCK"] = Address;
+            using var addProcess = Process.Start(psi)!;
+            addProcess.WaitForExit();
+            string stderr = addProcess.StandardError.ReadToEnd().Trim();
+            if (stderr.Length != 0)
+            {
+                Console.WriteLine("ssh-add stderr: " + stderr);
+            }
+            Assert.Equal(0, addProcess.ExitCode);
+        }
+
+        public void Dispose()
+        {
+            if (_sshAgentProcess is not null)
+            {
+                try
+                {
+                    _sshAgentProcess.Kill();
+                    string stderr = _sshAgentProcess.StandardError.ReadToEnd().Trim();
+                    if (stderr.Length != 0)
+                    {
+                        Console.WriteLine("ssh-agent stderr: " + stderr);
+                    }
+                    _sshAgentProcess.WaitForExit();
+                }
+                catch
+                { }
+            }
+            try
+            {
+                File.Delete(Address);
+            }
+            catch
+            {}
+        }
+    }
+}
