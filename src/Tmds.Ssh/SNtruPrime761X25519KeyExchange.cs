02a25cf685b2beded2132c7657c7f46c1ccdd8c7
Scott Xu <scott-xu@msn.com>
2025-01-10 08:52:56 +0800

    Rename

--- src/Tmds.Ssh/SNtruPrime761X25519KeyExchange.cs
+++ src/Tmds.Ssh/SNtruPrime761X25519KeyExchange.cs
@@ -13,9 +13,9 @@ using Org.BouncyCastle.Security;
 
 namespace Tmds.Ssh;
 
-// Key Exchange Method Using Hybrid Streamlined NTRU Prime sntrup761 and X25519 with SHA-512
+// Key Exchange Method Using Hybrid Streamlined NTRU Prime sntrup761 and X25519
 // https://www.ietf.org/archive/id/draft-ietf-sshm-ntruprime-ssh-01.html
-sealed class SNtruPrime761X25519Sha512KeyExchange : Curve25519KeyExchange
+sealed class SNtruPrime761X25519KeyExchange : Curve25519KeyExchange
 {
     private readonly SNtruPrimeParameters _sntruPrimeParameters = SNtruPrimeParameters.sntrup761;
     private readonly HashAlgorithmName _hashAlgorithmName = HashAlgorithmName.SHA512;
