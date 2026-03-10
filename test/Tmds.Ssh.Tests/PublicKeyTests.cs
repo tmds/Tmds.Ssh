cb525f42fe6a17774cd8a19067edd3e45ca28223
Tom Deseyn <tom.deseyn@gmail.com>
2026-03-10 22:53:01 +0100

    PublicKey: fix Equals always returning false. (#454)

--- test/Tmds.Ssh.Tests/PublicKeyTests.cs
+++ test/Tmds.Ssh.Tests/PublicKeyTests.cs
@@ -19,4 +19,54 @@ public class PublicKeyTests
         string expected = $"{typeName} {Convert.ToBase64String(rawData)}";
         Assert.Equal(expected, publicKey.ToString());
     }
+
+    [Fact]
+    public void EqualKeysAreEqual()
+    {
+        string typeName = "ecdh-sha2-nistp256";
+        byte[] rawData = new byte[64];
+        Random.Shared.NextBytes(rawData);
+
+        PublicKey key1 = new PublicKey(typeName, rawData);
+        PublicKey key2 = new PublicKey(typeName, rawData);
+
+        Assert.True(key1.Equals(key2));
+        Assert.True(key2.Equals(key1));
+        Assert.Equal(key1.GetHashCode(), key2.GetHashCode());
+    }
+
+    [Fact]
+    public void DifferentRawDataAreNotEqual()
+    {
+        string typeName = "ecdh-sha2-nistp256";
+        byte[] rawData1 = new byte[64];
+        byte[] rawData2 = new byte[64];
+        Random.Shared.NextBytes(rawData1);
+        Random.Shared.NextBytes(rawData2);
+
+        PublicKey key1 = new PublicKey(typeName, rawData1);
+        PublicKey key2 = new PublicKey(typeName, rawData2);
+
+        Assert.False(key1.Equals(key2));
+    }
+
+    [Fact]
+    public void DifferentTypesAreNotEqual()
+    {
+        byte[] rawData = new byte[64];
+        Random.Shared.NextBytes(rawData);
+
+        PublicKey key1 = new PublicKey("ecdh-sha2-nistp256", rawData);
+        PublicKey key2 = new PublicKey("ssh-rsa", rawData);
+
+        Assert.False(key1.Equals(key2));
+    }
+
+    [Fact]
+    public void EqualsNullReturnsFalse()
+    {
+        PublicKey key = new PublicKey("ecdh-sha2-nistp256", new byte[64]);
+
+        Assert.False(key.Equals(null));
+    }
 }
